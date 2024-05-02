1fb3a2e855c315a2ec2a6f1ae0f46ee8319478a2
lucasbaldan <lucasbaldan2014@gmail.com>
2024-05-02 20:59:57 -0300

    Configurando o slim
    não mexer nos arquivos

--- index.php
+++ index.php
@@ -1,17 +1,22 @@
 <?php
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
-use Slim\Exception\NotFoundException;
 use Slim\Factory\AppFactory;
 
 require __DIR__ . '/vendor/autoload.php';
 
 $app = AppFactory::create();
+$app->setBasePath('/veterinariaUNESC');
 $app->addErrorMiddleware(true, true, true);
 
 
 
-$app->get('/IC/veterinariaUNESC/', function (Request $request, Response $response, $args) {
+$app->get('/', function (Request $request, Response $response, $args) {
+    $response->getBody()->write("Hello world!");
+    return $response;
+});
+
+$app->get('/cadDiagnostico', function (Request $request, Response $response, $args) {
     $response->getBody()->write("Hello world!");
     return $response;
 });
