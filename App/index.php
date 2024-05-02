347d1371fe900b1f8bee5a9f42f9cc592755da9d
Lucas Faé Baldan <lucasbaldan2014@gmail.com>
2024-05-01 23:29:16 -0300

    Instalado o slim no projeto
    fiquei um dia inteiro pra conseguir que essa porcaria funcionasse e ainda está bugado, que Desgraaaaaaaa

--- App/index.php
+++ App/index.php
@@ -1,18 +1,30 @@
 <?php
 
+namespace App\Views;
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
 require '../vendor/autoload.php';
 
-?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h2>EAE VETERINARIA</h2>
-    
-</body>
-</html>
+class ViewTeste{
+
+    public static function hello(Request $request, Response $response, $args)
+    {
+        $response = '<!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Document</title>
+        </head>
+        <body>
+            <h2>EAE VETERINARIA</h2>
+            
+        </body>
+        </html>';
+        return $response;
+    }
+}
+?>
