8d4c8e995e212a8f7eef8797adb21ba1bb124a9f
lucasbaldan <lucasbaldan2014@gmail.com>
2024-05-03 16:53:14 -0300

    Agrupamento de rotas do slim

--- index.php
+++ index.php
@@ -1,24 +1,33 @@
 <?php
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
+
 use Slim\Factory\AppFactory;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Server\RequestHandlerInterface;
+use Slim\Routing\RouteCollectorProxy;
 
 require __DIR__ . '/vendor/autoload.php';
 
 $app = AppFactory::create();
 $app->setBasePath('/veterinariaUNESC');
+$app->addRoutingMiddleware();
 $app->addErrorMiddleware(true, true, true);
 
+// Definição de um grupo de rotas /pessoas
+$app->group('/pessoas', function (RouteCollectorProxy $group) {
 
+    // Definição das rotas dentro do grupo /pessoas
 
-$app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello world!");
-    return $response;
-});
+    $group->get('/teste',  App\Controllers\Pessoas::class. ':exibir');
 
-$app->get('/cadDiagnostico', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello world!");
-    return $response;
+})->add(function (Request $request, RequestHandlerInterface $handler) {
+    $uri = $request->getUri()->getPath();
+     if (!in_array($uri, ['/veterinariaUNESC/pessoas/teste'])) {
+         $response = new \Slim\Psr7\Response();
+         $response->getBody()->write(json_encode(["retorno" => false, "mensagem" => 'A requisição foi efetuada de maneira incorreta.']));
+         return $response->withStatus(400)->withHeader('Content-Type', 'application/json');
+     }
+
+    return $handler->handle($request);
 });
 
 $app->run();
