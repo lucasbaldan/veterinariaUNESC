32561e7f1b57e1604a9e1207bf0dc8f6d95cda13
Lucas Faé Baldan <lucasbaldan2014@gmail.com>
2024-05-05 16:49:19 -0300

    Configuração do twig com slim
    renderização de páginas com html

---
+++ App/Views/Views/LoginPage.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Views;
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Views\Twig;
+
+class LoginPage
+{
+    private $twig;
+
+    public function __construct(Twig $twig)
+    {
+        $this->twig = $twig;
+    }
+
+    public function exibir(Request $request, Response $response, $args)
+    {
+        // Renderiza a tela de login usando o Twig
+        return $this->twig->render($response, 'login.twig');
+    }
+}
--- index.php
+++ index.php
@@ -1,9 +1,14 @@
 <?php
 
 use Slim\Factory\AppFactory;
+use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Server\RequestHandlerInterface;
 use Slim\Routing\RouteCollectorProxy;
+use Slim\Views\Twig;
+use Slim\Views\TwigMiddleware;
+
+
 
 require __DIR__ . '/vendor/autoload.php';
 
@@ -12,20 +17,41 @@ $app->setBasePath('/veterinariaUNESC');
 $app->addRoutingMiddleware();
 $app->addErrorMiddleware(true, true, true);
 
-// Definição de um grupo de rotas /pessoas
-$app->group('/pessoas', function (RouteCollectorProxy $group) {
+/////////////////////// ROTAS DE REQUISIÇÕES PARA PROCESSAMENTO DE TELAS
+
+$twig = Twig::create(__DIR__ . '/App/Views/Templates', ['cache' => false]);
+$app->add(TwigMiddleware::create($app, $twig));
+
+$app->group('/paginas', function (RouteCollectorProxy $group) use ($twig) {
+    $group->get('/login', function (Request $request, Response $response, $args) use ($twig) {
+        // Renderiza a página de login usando o Twig
+        return $twig->render($response, 'login.twig');
+    });
+})->add(function (Request $request, RequestHandlerInterface $handler) {
+    $uri = $request->getUri()->getPath();
+    if (!in_array($uri, ['/veterinariaUNESC/paginas/login'])) {
+        $response = new \Slim\Psr7\Response();
+        $response->getBody()->write(json_encode(["retorno" => false, "mensagem" => 'A requisicao foi efetuada de maneira incorreta.']));
+        return $response->withStatus(400)->withHeader('Content-Type', 'application/json');
+    }
+
+    return $handler->handle($request);
+});
+
+/////////////////////// ROTAS DE REQUISIÇÕES PARA PROCESSAMENTO DE BACKEND
+
+$app->group('/server', function (RouteCollectorProxy $group) {
 
     // Definição das rotas dentro do grupo /pessoas
 
-    $group->get('/teste',  App\Controllers\Pessoas::class. ':exibir');
-
+    $group->post('/teste',  App\Controllers\Pessoas::class . ':exibir');
 })->add(function (Request $request, RequestHandlerInterface $handler) {
     $uri = $request->getUri()->getPath();
-     if (!in_array($uri, ['/veterinariaUNESC/pessoas/teste'])) {
-         $response = new \Slim\Psr7\Response();
-         $response->getBody()->write(json_encode(["retorno" => false, "mensagem" => 'A requisição foi efetuada de maneira incorreta.']));
-         return $response->withStatus(400)->withHeader('Content-Type', 'application/json');
-     }
+    if (!in_array($uri, ['/veterinariaUNESC/pessoas/teste'])) {
+        $response = new \Slim\Psr7\Response();
+        $response->getBody()->write(json_encode(["retorno" => false, "mensagem" => 'A requisição foi efetuada de maneira incorreta.']));
+        return $response->withStatus(400)->withHeader('Content-Type', 'application/json');
+    }
 
     return $handler->handle($request);
 });
