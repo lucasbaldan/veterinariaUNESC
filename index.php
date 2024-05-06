9fe893e15b1fb355adf37cea34c30b43bd2e90f0
Lucas Faé Baldan <lucasbaldan2014@gmail.com>
2024-05-05 22:00:35 -0300

    Finalização da configuração do twig com slim
    Renderização de templates com twig por pelos controladores das views

--- index.php
+++ index.php
@@ -8,8 +8,6 @@ use Slim\Routing\RouteCollectorProxy;
 use Slim\Views\Twig;
 use Slim\Views\TwigMiddleware;
 
-
-
 require __DIR__ . '/vendor/autoload.php';
 
 $app = AppFactory::create();
@@ -17,19 +15,22 @@ $app->setBasePath('/veterinariaUNESC');
 $app->addRoutingMiddleware();
 $app->addErrorMiddleware(true, true, true);
 
-/////////////////////// ROTAS DE REQUISIÇÕES PARA PROCESSAMENTO DE TELAS
-
 $twig = Twig::create(__DIR__ . '/App/Views/Templates', ['cache' => false]);
 $app->add(TwigMiddleware::create($app, $twig));
 
+/////////////////////// ROTAS DE REQUISIÇÕES PARA PROCESSAMENTO DE TELAS
+
 $app->group('/paginas', function (RouteCollectorProxy $group) use ($twig) {
+
     $group->get('/login', function (Request $request, Response $response, $args) use ($twig) {
-        // Renderiza a página de login usando o Twig
-        return $twig->render($response, 'login.twig');
+        $tela =  new App\Views\LoginPage($twig);
+        return $tela->exibir($request, $response, $args);
     });
+
+
 })->add(function (Request $request, RequestHandlerInterface $handler) {
     $uri = $request->getUri()->getPath();
-    if (!in_array($uri, ['/veterinariaUNESC/paginas/login'])) {
+    if (!in_array($uri, ['/veterinariaUNESC/paginas/login', '/veterinariaUNESC/paginas/outra'])) {
         $response = new \Slim\Psr7\Response();
         $response->getBody()->write(json_encode(["retorno" => false, "mensagem" => 'A requisicao foi efetuada de maneira incorreta.']));
         return $response->withStatus(400)->withHeader('Content-Type', 'application/json');
@@ -44,10 +45,10 @@ $app->group('/server', function (RouteCollectorProxy $group) {
 
     // Definição das rotas dentro do grupo /pessoas
 
-    $group->post('/teste',  App\Controllers\Pessoas::class . ':exibir');
+    $group->get('/teste',  App\Controllers\Pessoas::class . ':exibir');
 })->add(function (Request $request, RequestHandlerInterface $handler) {
     $uri = $request->getUri()->getPath();
-    if (!in_array($uri, ['/veterinariaUNESC/pessoas/teste'])) {
+    if (!in_array($uri, ['/veterinariaUNESC/server/teste'])) {
         $response = new \Slim\Psr7\Response();
         $response->getBody()->write(json_encode(["retorno" => false, "mensagem" => 'A requisição foi efetuada de maneira incorreta.']));
         return $response->withStatus(400)->withHeader('Content-Type', 'application/json');
