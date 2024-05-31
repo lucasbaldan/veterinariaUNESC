1759e3b5e4924440ed4e47a02ac4050bf2e65861
CMorethson <morethsoncarlos@gmail.com>
2024-05-31 11:41:19 -0300

    Cadastro de pessoas e outros
    Incluído a página de cadastro de pessoas, correção das rotas no index e algumas mudanças na ficha LPV

--- index.php
+++ index.php
@@ -48,13 +48,19 @@ $app->group('/paginas', function (RouteCollectorProxy $group) use ($twig) {
         return $tela->exibir($request, $response, $args);
     });
 
-
+    $group->get('/cadastroPessoas', function (Request $request, Response $response, $args) use ($twig) {
+        $tela =  new App\Views\CadastroPessoas($twig);
+        return $tela->exibir($request, $response, $args);
+    });
 })->add(function (Request $request, RequestHandlerInterface $handler) {
     $uri = $request->getUri()->getPath();
-    if (!in_array($uri, ['/veterinariaUNESC/paginas/login', 
-                         '/veterinariaUNESC/paginas/formularioLPV', 
-                         '/veterinariaUNESC/paginas/inicial', 
-                         '/veterinariaUNESC/paginas/listTipoAnimal'])) {
+    if (!in_array($uri, [
+        '/veterinariaUNESC/paginas/login',
+        '/veterinariaUNESC/paginas/formularioLPV',
+        '/veterinariaUNESC/paginas/inicial',
+        '/veterinariaUNESC/paginas/listTipoAnimal',
+        '/veterinariaUNESC/paginas/cadastroPessoas'
+    ])) {
         $response = new \Slim\Psr7\Response();
         $response->getBody()->write(json_encode(["retorno" => false, "mensagem" => 'A requisicao foi efetuada de maneira incorreta.']));
         return $response->withStatus(400)->withHeader('Content-Type', 'application/json');
@@ -75,10 +81,50 @@ $app->group('/server', function (RouteCollectorProxy $group) {
         $pessoasGroup->post('/grid', App\Controllers\TiposAnimais::class . ':montarGrid');
     });
 
+    $group->group('/gruposUsuarios', function (RouteCollectorProxy $GrUsuariosGroup) {
+        $GrUsuariosGroup->post('/salvaGrupoUsuarios',  App\Controllers\GruposUsuarios::class . ':Salvar');
+        $GrUsuariosGroup->post('/excluiGruposUsuarios',  App\Controllers\GruposUsuarios::class . ':ExcluirGruposUsuarios');
+        $GrUsuariosGroup->post('/retornaGruposUsuarios',  App\Controllers\GruposUsuarios::class . ':RetornarGruposUsuarios');
+        $GrUsuariosGroup->post('/retornaDadosGrupoUsuarios',  App\Controllers\GruposUsuarios::class . ':RetornarDadosGrupoUsuario');
+    });
+
+    $group->group('/usuarios', function (RouteCollectorProxy $usuariosGroup) {
+        $usuariosGroup->post('/salvaUsuario',  App\Controllers\Usuarios::class . ':Salvar');
+        $usuariosGroup->post('/excluiUsuario',  App\Controllers\Usuarios::class . ':ExcluirUsuario');
+        $usuariosGroup->post('/retornaUsuarios',  App\Controllers\Usuarios::class . ':RetornarUsuarios');
+        $usuariosGroup->post('/retornaDadosUsuario',  App\Controllers\Usuarios::class . ':RetornarDadosUsuario');
+        $usuariosGroup->post('/ativaDesativaUsuario',  App\Controllers\Usuarios::class . ':AtivarDesativarUsuario');
+    });
+
+    $group->group('/fichaLPV', function (RouteCollectorProxy $fichaLPVGroup) {
+        $fichaLPVGroup->post('/salvafichaLPV',  App\Controllers\FormularioLPV::class . ':Salvar');
+        $fichaLPVGroup->post('/retornaFichasLPV',  App\Controllers\FormularioLPV::class . ':RetornarFichasLPV');
+        $fichaLPVGroup->post('/retornaDadosFichaLPV',  App\Controllers\FormularioLPV::class . ':RetornarDadosFichaLPV');
+        $fichaLPVGroup->post('/apagaFichaLPV',  App\Controllers\FormularioLPV::class . ':ApagarFichaLPV');
+    });
+
 })->add(function (Request $request, RequestHandlerInterface $handler) {
     $uri = $request->getUri()->getPath();
-    if (!in_array($uri, ['/veterinariaUNESC/server/pessoas/login',
-                         '/veterinariaUNESC/server/tipoAnimal/grid'])) {
+    if (!in_array($uri, [
+        '/veterinariaUNESC/server/pessoas/login',
+        '/veterinariaUNESC/server/tipoAnimal/grid',
+
+        '/veterinariaUNESC/server/gruposUsuarios/salvaGrupoUsuarios',
+        '/veterinariaUNESC/server/gruposUsuarios/excluiGruposUsuarios',
+        '/veterinariaUNESC/server/gruposUsuarios/retornaGruposUsuarios',
+        '/veterinariaUNESC/server/gruposUsuarios/retornaDadosGrupoUsuarios',
+
+        '/veterinariaUNESC/server/usuarios/salvaUsuario',
+        '/veterinariaUNESC/server/usuarios/excluiUsuario',
+        '/veterinariaUNESC/server/usuarios/retornaUsuarios',
+        '/veterinariaUNESC/server/usuarios/retornaDadosUsuario',
+        '/veterinariaUNESC/server/usuarios/ativaDesativaUsuario',
+
+        '/veterinariaUNESC/server/fichaLPV/salvafichaLPV',
+        '/veterinariaUNESC/server/fichaLPV/retornaFichasLPV',
+        '/veterinariaUNESC/server/fichaLPV/retornaDadosFichaLPV',
+        '/veterinariaUNESC/server/fichaLPV/apagaFichaLPV',
+    ])) {
         $response = new \Slim\Psr7\Response();
         $response->getBody()->write(json_encode(["retorno" => false, "mensagem" => 'A requisição foi efetuada de maneira incorreta.']));
         return $response->withStatus(400)->withHeader('Content-Type', 'application/json');
@@ -87,70 +133,48 @@ $app->group('/server', function (RouteCollectorProxy $group) {
     return $handler->handle($request);
 });
 
-$app->group('/fichaLPV', function (RouteCollectorProxy $group) {
+// $app->group('/gruposUsuarios', function (RouteCollectorProxy $group) {
 
-    $group->post('/salvaFichaLPV',  App\Controllers\FormularioLPV::class . ':Salvar');
+//     $group->post('/salvaGrupoUsuarios',  App\Controllers\GruposUsuarios::class . ':Salvar');
 
-    $group->post('/retornaFichaLPV',  App\Controllers\FormularioLPV::class . ':RetornarFichasLPV');
+//     $group->post('/excluiGruposUsuarios',  App\Controllers\GruposUsuarios::class . ':ExcluirGruposUsuarios');
 
-    $group->post('/retornaDadosFichaLPV',  App\Controllers\FormularioLPV::class . ':RetornarDadosFichaLPV');
-    
-    $group->post('/apagaFichaLPV',  App\Controllers\FormularioLPV::class . ':ApagarFichaLPV');
-    
-  
-})->add(function (Request $request, RequestHandlerInterface $handler) {
-    $uri = $request->getUri()->getPath();
-    if (!in_array($uri, ['/veterinariaUNESC/fichaLPV/salvaFichaLPV', '/veterinariaUNESC/fichaLPV/retornaFichaLPV', '/veterinariaUNESC/fichaLPV/retornaDadosFichaLPV', '/veterinariaUNESC/fichaLPV/apagaFichaLPV'])) {
-        $response = new \Slim\Psr7\Response();
-        $response->getBody()->write(json_encode(["retorno" => false, "mensagem" => 'A requisição foi efetuada de maneira incorreta.']));
-        return $response->withStatus(400)->withHeader('Content-Type', 'application/json');
-    }
+//     $group->post('/retornaGruposUsuarios',  App\Controllers\GruposUsuarios::class . ':RetornarGruposUsuarios');
 
-    return $handler->handle($request);
-});
+//     $group->post('/retornaDadosGrupoUsuarios',  App\Controllers\GruposUsuarios::class . ':RetornarDadosGrupoUsuario');
 
-$app->group('/gruposUsuarios', function (RouteCollectorProxy $group) {
+// })->add(function (Request $request, RequestHandlerInterface $handler) {
+//     $uri = $request->getUri()->getPath();
+//     if (!in_array($uri, ['/veterinariaUNESC/gruposUsuarios/salvaGrupoUsuarios', '/veterinariaUNESC/gruposUsuarios/excluiGruposUsuarios', '/veterinariaUNESC/gruposUsuarios/retornaGruposUsuarios', '/veterinariaUNESC/gruposUsuarios/retornaDadosGrupoUsuarios'])) {
+//         $response = new \Slim\Psr7\Response();
+//         $response->getBody()->write(json_encode(["retorno" => false, "mensagem" => 'A requisição foi efetuada de maneira incorreta.']));
+//         return $response->withStatus(400)->withHeader('Content-Type', 'application/json');
+//     }
 
-    $group->post('/salvaGrupoUsuarios',  App\Controllers\GruposUsuarios::class . ':Salvar');
-    
-    $group->post('/excluiGruposUsuarios',  App\Controllers\GruposUsuarios::class . ':ExcluirGruposUsuarios');
-    
-    $group->post('/retornaGruposUsuarios',  App\Controllers\GruposUsuarios::class . ':RetornarGruposUsuarios');
-    
-    $group->post('/retornaDadosGrupoUsuarios',  App\Controllers\GruposUsuarios::class . ':RetornarDadosGrupoUsuario');
+//     return $handler->handle($request);
+// });
 
-})->add(function (Request $request, RequestHandlerInterface $handler) {
-    $uri = $request->getUri()->getPath();
-    if (!in_array($uri, ['/veterinariaUNESC/gruposUsuarios/salvaGrupoUsuarios', '/veterinariaUNESC/gruposUsuarios/excluiGruposUsuarios', '/veterinariaUNESC/gruposUsuarios/retornaGruposUsuarios', '/veterinariaUNESC/gruposUsuarios/retornaDadosGrupoUsuarios'])) {
-        $response = new \Slim\Psr7\Response();
-        $response->getBody()->write(json_encode(["retorno" => false, "mensagem" => 'A requisição foi efetuada de maneira incorreta.']));
-        return $response->withStatus(400)->withHeader('Content-Type', 'application/json');
-    }
+// $app->group('/usuarios', function (RouteCollectorProxy $group) {
 
-    return $handler->handle($request);
-});
+//     $group->post('/salvaUsuario',  App\Controllers\Usuarios::class . ':Salvar');
 
-$app->group('/usuarios', function (RouteCollectorProxy $group) {
-    
-    $group->post('/salvaUsuario',  App\Controllers\Usuarios::class . ':Salvar');
+//     $group->post('/excluiUsuario',  App\Controllers\Usuarios::class . ':ExcluirUsuario');
 
-    $group->post('/excluiUsuario',  App\Controllers\Usuarios::class . ':ExcluirUsuario');
+//     $group->post('/retornaUsuarios',  App\Controllers\Usuarios::class . ':RetornarUsuarios');
 
-    $group->post('/retornaUsuarios',  App\Controllers\Usuarios::class . ':RetornarUsuarios');
+//     $group->post('/retornaDadosUsuario',  App\Controllers\Usuarios::class . ':RetornarDadosUsuario');
 
-    $group->post('/retornaDadosUsuario',  App\Controllers\Usuarios::class . ':RetornarDadosUsuario');
+//     $group->post('/ativaDesativaUsuario',  App\Controllers\Usuarios::class . ':AtivarDesativarUsuario');
 
-    $group->post('/ativaDesativaUsuario',  App\Controllers\Usuarios::class . ':AtivarDesativarUsuario');
+// })->add(function (Request $request, RequestHandlerInterface $handler) {
+//     $uri = $request->getUri()->getPath();
+//     if (!in_array($uri, ['/veterinariaUNESC/usuarios/salvaUsuario', '/veterinariaUNESC/usuarios/excluiUsuario', '/veterinariaUNESC/usuarios/retornaUsuarios', '/veterinariaUNESC/usuarios/retornaDadosUsuario', '/veterinariaUNESC/usuarios/ativaDesativaUsuario'])) {
+//         $response = new \Slim\Psr7\Response();
+//         $response->getBody()->write(json_encode(["retorno" => false, "mensagem" => 'A requisição foi efetuada de maneira incorreta.']));
+//         return $response->withStatus(400)->withHeader('Content-Type', 'application/json');
+//     }
 
-})->add(function (Request $request, RequestHandlerInterface $handler) {
-    $uri = $request->getUri()->getPath();
-    if (!in_array($uri, ['/veterinariaUNESC/usuarios/salvaUsuario', '/veterinariaUNESC/usuarios/excluiUsuario', '/veterinariaUNESC/usuarios/retornaUsuarios', '/veterinariaUNESC/usuarios/retornaDadosUsuario', '/veterinariaUNESC/usuarios/ativaDesativaUsuario'])) {
-        $response = new \Slim\Psr7\Response();
-        $response->getBody()->write(json_encode(["retorno" => false, "mensagem" => 'A requisição foi efetuada de maneira incorreta.']));
-        return $response->withStatus(400)->withHeader('Content-Type', 'application/json');
-    }
-
-    return $handler->handle($request);
-});
+//     return $handler->handle($request);
+// });
 
 $app->run();
--- public/js/formularioLPV.js
+++ public/js/formularioLPV.js
@@ -1,82 +1,116 @@
-$(document).ready(function() {
-            $('#ExpandFicha').click(function() {
-                $('.accordion-collapse').each(function() {
-                    let collapseInstance = new bootstrap.Collapse(this, {
-                        toggle: false
-                    });
-                    collapseInstance.show(); 
-                });
+$(document).ready(function () {
+    const notificacao = new Notificacao({
+        duration: 10000,
+        position: {
+            x: 'center',
+            y: 'bottom',
+        },
+    });
+
+    $('#ExpandFicha').click(function () {
+        $('.accordion-collapse').each(function () {
+            let collapseInstance = new bootstrap.Collapse(this, {
+                toggle: false
             });
+            collapseInstance.show();
+        });
+    });
 
-
-    $('#btnSalvarFormularioLPV').on('click', function (event) {
-
+    $('#formFichaLPV').submit(function (event) {
         event.preventDefault();
-        // var formData = $(this).serialize();
 
-        // console.log('FORMULARIO: ', formData);
-        // return;
+        var dadosForm = $(this).serialize();
 
         $.ajax({
-            type: "POST",
-            data: {
-                cdFichaLPV: $('#cdFichaLPV').val(),
-                dtFicha: $('#dtFicha').val(),
-                animal: $('#animal').val(),
-                nmVeterinarioRemetente: $('#nmVeterinarioRemetente').val(),
-                crmvVeterinarioRemetente: $('#crmvVeterinarioRemetente').val(),
-                nrTelVeterinarioRemetente: $('#nrTelVeterinarioRemetente').val(),
-                dsEmailVeterinarioRemetente: $('#dsEmailVeterinarioRemetente').val(),
-                nmCidadeVeterinarioRemetente: $('#nmCidadeVeterinarioRemetente').val(),
-                // cdUsuarioPlantonista : $('#cdFichaLPV'),
-                nmProprietario: $('#nmProprietario').val(),
-                nrTelefoneProprietario: $('#nrTelefoneProprietario').val(),
-                cidadePropriedade: $('#cidadePropriedade').val(),
-                dsEspecie: $('#dsEspecie').val(),
-                dsRaca: $('#dsRaca').val(),
-                dsSexo: $('#dsSexo').val(),
-                idade: $('#idade').val(),
-                totalAnimais: $('#totalAnimais').val(),
-                qtdAnimaisDoentes: $('#qtdAnimaisDoentes').val(),
-                qtdAnimaisMortos: $('#qtdAnimaisMortos').val(),
-                dsMaterialRecebido: $('#dsMaterialRecebido').val(),
-                dsDiagnosticoPresuntivo: $('#dsDiagnosticoPresuntivo'),
-                flAvaliacaoTumoralComMargem: $('#flAvaliacaoTumoralComMargem').val(),
-                dsNomeAnimal: $('#dsNomeAnimal').val(),
-                dsEpidemiologiaHistoriaClinica: $('#dsEpidemiologiaHistoriaClinica').val(),
-                dsLesoesMacroscopicas: $('#dsLesoesMacroscopicas').val(),
-                dsLesoesHistologicas: $('#dsLesoesHistologicas').val(),
-                dsDiagnostico: $('#dsDiagnostico').val(),
-                dsRelatorio: $('#dsRelatorio').val(),
+            url: '/veterinariaUNESC/server/fichaLPV/salvafichaLPV',
+            type: 'POST',
+            dataType: 'json',
+            data: dadosForm,
+            beforeSend: function () {
+                Loading.on();
             },
-            url: window.HOME + 'Controllers/formulario-lpv/salvar',
-            beforeSend: function () { },
-            complete: function () { },
-            success: function (data) {
-                $("#ResultDados").html(data);
+            success: function (response) {
+                alert(response);
+            },
+            error: function (xhr, status, error) {
+                // console.log('ERRROOOOO: ', xhr.response);
+                notificacao.push(xhr.responseJSON.MESSAGE, 'warning');
+            },
+            complete: function () {
+                Loading.off();
             }
         });
-
-        event.preventDefault();
-        var validarTela = false;
-        if ($('#usuario').val() === '') {
-            $('#usuario').addClass('is-invalid');
-            validarTela = true;
-        }
-        else {
-            $('#usuario').removeClass('is-invalid');
-        }
-        if ($('#senha').val() === '') {
-            $('#senha').addClass('is-invalid');
-            validarTela = true;
-        } else {
-            $('#senha').removeClass('is-invalid');
-        }
-
-        if (validarTela) {
-            return;
-        }
-        //this.submit();
-        notificacao.push("<b>Usuário ou senha inválidos</b><br><br> Por favor verifique os dados de acesso e tente novamente.", 'warning');
     });
+
+
+    // $('#btnSalvarFormularioLPV').on('click', function (event) {
+
+    //     event.preventDefault();
+    //     // var formData = $(this).serialize();
+
+    //     // console.log('FORMULARIO: ', formData);
+    //     // return;
+
+    //     $.ajax({
+    //         type: "POST",
+    //         data: {
+    //             cdFichaLPV: $('#cdFichaLPV').val(),
+    //             dtFicha: $('#dtFicha').val(),
+    //             animal: $('#animal').val(),
+    //             nmVeterinarioRemetente: $('#nmVeterinarioRemetente').val(),
+    //             crmvVeterinarioRemetente: $('#crmvVeterinarioRemetente').val(),
+    //             nrTelVeterinarioRemetente: $('#nrTelVeterinarioRemetente').val(),
+    //             dsEmailVeterinarioRemetente: $('#dsEmailVeterinarioRemetente').val(),
+    //             nmCidadeVeterinarioRemetente: $('#nmCidadeVeterinarioRemetente').val(),
+    //             // cdUsuarioPlantonista : $('#cdFichaLPV'),
+    //             nmProprietario: $('#nmProprietario').val(),
+    //             nrTelefoneProprietario: $('#nrTelefoneProprietario').val(),
+    //             cidadePropriedade: $('#cidadePropriedade').val(),
+    //             dsEspecie: $('#dsEspecie').val(),
+    //             dsRaca: $('#dsRaca').val(),
+    //             dsSexo: $('#dsSexo').val(),
+    //             idade: $('#idade').val(),
+    //             totalAnimais: $('#totalAnimais').val(),
+    //             qtdAnimaisDoentes: $('#qtdAnimaisDoentes').val(),
+    //             qtdAnimaisMortos: $('#qtdAnimaisMortos').val(),
+    //             dsMaterialRecebido: $('#dsMaterialRecebido').val(),
+    //             dsDiagnosticoPresuntivo: $('#dsDiagnosticoPresuntivo'),
+    //             flAvaliacaoTumoralComMargem: $('#flAvaliacaoTumoralComMargem').val(),
+    //             dsNomeAnimal: $('#dsNomeAnimal').val(),
+    //             dsEpidemiologiaHistoriaClinica: $('#dsEpidemiologiaHistoriaClinica').val(),
+    //             dsLesoesMacroscopicas: $('#dsLesoesMacroscopicas').val(),
+    //             dsLesoesHistologicas: $('#dsLesoesHistologicas').val(),
+    //             dsDiagnostico: $('#dsDiagnostico').val(),
+    //             dsRelatorio: $('#dsRelatorio').val(),
+    //         },
+    //         url: 'verinariaUNESC/fichaLPV/salvar',
+    //         beforeSend: function () { },
+    //         complete: function () { },
+    //         success: function (data) {
+    //             $("#ResultDados").html(data);
+    //         }
+    //     });
+
+    //     event.preventDefault();
+    //     var validarTela = false;
+    //     if ($('#usuario').val() === '') {
+    //         $('#usuario').addClass('is-invalid');
+    //         validarTela = true;
+    //     }
+    //     else {
+    //         $('#usuario').removeClass('is-invalid');
+    //     }
+    //     if ($('#senha').val() === '') {
+    //         $('#senha').addClass('is-invalid');
+    //         validarTela = true;
+    //     } else {
+    //         $('#senha').removeClass('is-invalid');
+    //     }
+
+    //     if (validarTela) {
+    //         return;
+    //     }
+    //     //this.submit();
+    //     notificacao.push("<b>Usuário ou senha inválidos</b><br><br> Por favor verifique os dados de acesso e tente novamente.", 'warning');
+    // });
 });
