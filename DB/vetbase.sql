6fdb233484473a031148644800aa35de7ed6799e
Lucas Faé Baldan <lucasbaldan2014@gmail.com>
2024-06-06 00:28:11 -0300

    bd

--- DB/vetbase.sql
+++ DB/vetbase.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 31/05/2024 às 03:43
+-- Tempo de geração: 06/06/2024 às 05:27
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
@@ -33,6 +33,25 @@ CREATE TABLE `acessos_tipos_usuarios` (
   `CD_TIPOS_ACESSOS_USUARIOS` int(11) DEFAULT NULL,
   `USUARIOS_CD_USUARIO` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `especies`
+--
+
+CREATE TABLE `especies` (
+  `CD_ESPECIE` int(11) NOT NULL,
+  `DESCRICAO` varchar(500) NOT NULL,
+  `FL_ATIVO` int(1) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Despejando dados para a tabela `especies`
+--
+
+INSERT INTO `especies` (`CD_ESPECIE`, `DESCRICAO`, `FL_ATIVO`) VALUES
+(1, 'Pastor-Alemão', 0);
 
 -- --------------------------------------------------------
 
@@ -117,6 +136,19 @@ CREATE TABLE `pessoas` (
 -- --------------------------------------------------------
 
 --
+-- Estrutura para tabela `racas`
+--
+
+CREATE TABLE `racas` (
+  `CD_RACA` int(11) NOT NULL,
+  `DESCRICAO` varchar(500) NOT NULL,
+  `FL_ATIVO` int(1) NOT NULL DEFAULT 1,
+  `CD_ESPECIE` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura para tabela `tipo_animal`
 --
 
@@ -131,16 +163,16 @@ CREATE TABLE `tipo_animal` (
 --
 
 INSERT INTO `tipo_animal` (`cd_tipo_animal`, `descricao`, `fl_ativo`) VALUES
-(1, 'Anta', 0),
-(2, 'Ariranha', 0),
-(3, 'Bicho-preguiça', 0),
-(4, 'Bugio', 0),
-(5, 'Cachorro', 0),
-(6, 'Cavalo', 0),
-(7, 'Coati', 0),
-(8, 'Cotia', 0),
-(9, 'Cutia', 0),
-(10, 'Esquilo', 0),
+(1, 'Anta', 1),
+(2, 'Ariranha', 1),
+(3, 'Bicho-preguiça', 1),
+(4, 'Bugio', 1),
+(5, 'Cachorro', 1),
+(6, 'Cavalo', 1),
+(7, 'Coati', 1),
+(8, 'Cotia', 1),
+(9, 'Cutia', 1),
+(10, 'Esquilo', 1),
 (11, 'Gato', 0),
 (12, 'Jaguatirica', 0),
 (13, 'Lobo-guará', 0),
@@ -149,11 +181,10 @@ INSERT INTO `tipo_animal` (`cd_tipo_animal`, `descricao`, `fl_ativo`) VALUES
 (16, 'Paca', 0),
 (17, 'Porco-do-mato', 0),
 (18, 'Quati', 0),
-(19, 'Raposa', 0),
-(20, 'Sagui', 0),
+(19, 'Raposa', 1),
 (21, 'Tamanduá', 0),
-(22, 'Tatu', 0),
-(23, 'Veado', 0);
+(30, 'Macaco Branco', 1),
+(42, 'Amazonense', 1);
 
 -- --------------------------------------------------------
 
@@ -188,6 +219,12 @@ ALTER TABLE `acessos_tipos_usuarios`
   ADD PRIMARY KEY (`CD_ACESSO_TIPO_USUARIO`);
 
 --
+-- Índices de tabela `especies`
+--
+ALTER TABLE `especies`
+  ADD PRIMARY KEY (`CD_ESPECIE`);
+
+--
 -- Índices de tabela `ficha_lpv`
 --
 ALTER TABLE `ficha_lpv`
@@ -212,10 +249,18 @@ ALTER TABLE `pessoas`
   ADD PRIMARY KEY (`CD_PESSOA`);
 
 --
+-- Índices de tabela `racas`
+--
+ALTER TABLE `racas`
+  ADD PRIMARY KEY (`CD_RACA`),
+  ADD KEY `FK_ESPECIE_RACAS` (`CD_ESPECIE`);
+
+--
 -- Índices de tabela `tipo_animal`
 --
 ALTER TABLE `tipo_animal`
-  ADD PRIMARY KEY (`cd_tipo_animal`);
+  ADD PRIMARY KEY (`cd_tipo_animal`),
+  ADD UNIQUE KEY `descricao` (`descricao`);
 
 --
 -- Índices de tabela `usuarios`
@@ -232,6 +277,12 @@ ALTER TABLE `usuarios`
 --
 ALTER TABLE `acessos_tipos_usuarios`
   MODIFY `CD_ACESSO_TIPO_USUARIO` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `especies`
+--
+ALTER TABLE `especies`
+  MODIFY `CD_ESPECIE` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de tabela `ficha_lpv`
@@ -258,16 +309,32 @@ ALTER TABLE `pessoas`
   MODIFY `CD_PESSOA` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT de tabela `racas`
+--
+ALTER TABLE `racas`
+  MODIFY `CD_RACA` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de tabela `tipo_animal`
 --
 ALTER TABLE `tipo_animal`
-  MODIFY `cd_tipo_animal` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `cd_tipo_animal` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=63;
 
 --
 -- AUTO_INCREMENT de tabela `usuarios`
 --
 ALTER TABLE `usuarios`
   MODIFY `CD_USUARIO` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Restrições para tabelas despejadas
+--
+
+--
+-- Restrições para tabelas `racas`
+--
+ALTER TABLE `racas`
+  ADD CONSTRAINT `FK_ESPECIE_RACAS` FOREIGN KEY (`CD_ESPECIE`) REFERENCES `especies` (`CD_ESPECIE`) ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
