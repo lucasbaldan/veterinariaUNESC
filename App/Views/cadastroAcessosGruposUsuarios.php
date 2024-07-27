efeea5a1853be53f98425f8efb5a27e705f7302a
Lucas Faé Baldan <lucasbaldan2014@gmail.com>
2024-07-26 22:32:15 -0300

    Update cadastroAcessosGruposUsuarios.php

--- App/Views/cadastroAcessosGruposUsuarios.php
+++ App/Views/cadastroAcessosGruposUsuarios.php
@@ -6,7 +6,7 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Views\Twig;
 
-class CadastroAcessosGruposUsuarios
+class CadastroAcessosGruposUsuarios 
 {
     private $twig;
     private $TelaComMenus;
